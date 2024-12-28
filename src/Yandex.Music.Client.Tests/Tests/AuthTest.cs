9296cb6e2b55606a9a01d2aa303f80912a6ebe80
Evgeniy <mail4evgeniy@gmail.com>
2024-12-28 19:10:17 +0300

    Revert "Исключена сборка под netstandard2.0"
    This reverts commit f18e91140147d43befd306213e62bec2d18b187f.

---
+++ src/Yandex.Music.Client.Tests/Tests/AuthTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Extensions.Ordering;
+
+namespace Yandex.Music.Client.Tests.Tests;
+
+[Collection("Yandex Test Harness"), Order(1)]
+[TestBeforeAfter]
+public class AuthTest : YandexTest
+{
+    public AuthTest(YandexTestHarness fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    [Order(0)]
+    public void GetQrLink_True()
+    {
+        var authSession = Fixture.Client.CreateAuthSession("evgeny.bogdan84");
+        var qrLink = Fixture.Client.GetAuthQRLink();
+
+        qrLink.Should().NotBeNull();
+    }
+}
